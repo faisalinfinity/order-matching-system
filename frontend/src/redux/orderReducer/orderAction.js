5b11780dea2317304a9bc47717fdae84ee15db41
faisalinfinity <mujtabafaisal944.fk@gmail.com>
2023-04-30 19:32:50 +0530

    completed logic part in action.js

--- frontend/src/redux/orderReducer/orderAction.js
+++ frontend/src/redux/orderReducer/orderAction.js
@@ -1,1 +1,54 @@
+import { BASE_URL } from "../../constants/constant";
+import axios from "axios";
+import {
+  GET_BUYER,
+  GET_BUYER_ERROR,
+  GET_BUYER_SUCCESS,
+  GET_SELLER,
+  GET_SELLER_ERROR,
+  GET_SELLER_SUCCESS,
+} from "./orderType";
 
+export const getBuyers = () => (dispatch) => {
+  dispatch({ type: GET_BUYER });
+  return axios
+    .get(`${BASE_URL}/orders/buyer`)
+    .then((res) => {
+      return dispatch({
+        type: GET_BUYER_SUCCESS,
+        payload: res.data,
+      });
+    })
+    .catch((err) => dispatch({ type: GET_BUYER_ERROR }));
+};
+
+export const getSellers = () => (dispatch) => {
+  dispatch({ type: GET_SELLER });
+  return axios
+    .get(`${BASE_URL}/orders/seller`)
+    .then((res) => {
+      return dispatch({
+        type: GET_SELLER_SUCCESS,
+        payload: res.data,
+      });
+    })
+    .catch((err) => dispatch({ type: GET_SELLER_ERROR }));
+};
+
+export const updateSeller = (id, changes) => (dispatch) => {
+  return axios
+    .patch(`${BASE_URL}/orders/seller/${id}`, changes)
+    .then((res) => {
+      getSellers();
+    })
+    .catch((err) => dispatch({ type: GET_SELLER_ERROR }));
+};
+
+export const updateBuyer = (id, changes) => (dispatch) => {
+  return axios
+    .patch(`${BASE_URL}/orders/buyer/${id}`, changes)
+    .then((res) => {
+      getBuyers();
+    })
+    .catch((err) => dispatch({ type: GET_SELLER_ERROR }));
+};
